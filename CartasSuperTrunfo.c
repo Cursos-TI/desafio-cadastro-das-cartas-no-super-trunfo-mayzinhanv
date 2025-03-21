f13f2bbe24f7ef296859767d9e289290cf68f121
mayzinhanv <maynv23@icloud.com>
2025-03-21 03:08:20 +0000

    Definição de variáveis e início do desenvolvimento

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,37 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Neste projeto, o intúito é gerar duas cartas de trunfo para cidades distintas. Cada cidade terá seus atributos.
+// Os questionamentos ao usuário devem ser claros, e as características finais devem ser apresentadas de acordo.
+
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+    char estado, codigo[2], nome[30];
+    int populacao, pturisticos;
+    float pib;
+    // as variáveis acima são referentes aos atributos da nossa carta. solicitaremos ao usuário que introduza as características
+    // levando à criação de cada uma de suas cartas.
+
+    printf("Bem-vindo ao desenvolvimento de cartas de Super Trunfo\n");
+    printf("Escolha uma letra entre A e H para representar o estado da sua carta 1 (a letra deve ser MAIÚSCULA): ");
+    scanf(" %c", &estado);
+    // no código acima o usuário define o ESTADO da carta, que deve ser uma letra entre A e H.
+    // a letra se enquadra como caractere, então devemos utilizar %c para categorizar a variável
+
+    printf ("Agora, selecione um número entre 01 e 04 para compor o código da sua carta 1 (lembre-se de digitar o 0): ");
+    scanf ("%s", codigo);
+    // agora o usuário foi instruido a escolher um número entre 01 e 04 para compor o código da carta.
+    // utiliza-se o %s visto que 2 caracteres formam uma string.
+
+    printf ("Defina o nome da cidade da carta 1 (o nome não deve ser composto, nem ultrapassar 30 caracteres): ");
+    scanf ("%s", nome);
+    // no código acima introduzimos o nome da cidade, informando ao usuário que não deve ser um nome composto.
+    // utiliza-se %s pois o nome é também uma string.
+    printf("DADOS DA CARTA 1\n");
+    printf("Estado: %c\n", estado);
+    printf("Código da carta: %c%s\n", estado, codigo);
+    printf ("Nome da Cidade: %s\n", nome);
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
@@ -20,3 +42,7 @@ int main() {
 
     return 0;
 }
+
+    // estado = char - codigo da carta = char[] - nome da cidade = char[] - populaçao = int - area em km = float
+    // PIB = float - numero de pontos turisticos = int
+
